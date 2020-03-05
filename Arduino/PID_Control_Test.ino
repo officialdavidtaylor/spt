8a6afbe6f5cae7f2ce809f1e02708b09b50345e2
David Taylor <dt@dt-mbp15.local>
2020-03-04 17:42:02 -0800

    template file

---
+++ Arduino/PID_Control_Test.ino
@@ -0,0 +1,24 @@
+/* File:    PID_Control_Test.ino
+ * Dev:     David Taylor
+ * Date:    2020-03-01
+ * Desc:    This code is designed to be run on an Arduino Mega2560, utilizing
+ *          an incoming data stream from an Adafruit BNO055 IMU. The final
+ *          application is my EECS 159 Senior Design Robot, the Smart Package
+ *          Transporter.
+ */
+
+#include <Wire.h>   // For I2C communications
+#include <PID.h>    // For easy PID implementation
+
+float
+pid = PID();
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
