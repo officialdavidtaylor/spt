5cd4020b9618d92f4bbf4d72d99825084cac3cdd
David Taylor <dt@dt-mbp15.local>
2020-03-05 19:29:31 -0800

    debugging timing error

--- RaspberryPi/serial_test.py
+++ RaspberryPi/serial_test.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import os, time
 
 GPIO.setmode(GPIO.BOARD)
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.5)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2.5)
 
 while True:
     rcv = port.read(20)
