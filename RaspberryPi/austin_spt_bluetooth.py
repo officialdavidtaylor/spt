4fe929b3d3e7eb5719a1d93025e95976b79d80f6
David Taylor <dt@dt-mbp15.local>
2020-03-07 13:34:23 -0800

    :|

--- RaspberryPi/austin_spt_bluetooth.py
+++ RaspberryPi/austin_spt_bluetooth.py
@@ -215,8 +215,8 @@ def __main__():
     try:
         while True:
             Remote.Update()
-            motors.ChangeSpeed((Remote.controllerAxes.axes["right_x"] *
-                R_X_AXIS_SCALE_VAL), (Remote.controllerAxes.axes["left_y"] *
+            motors.ChangeSpeed((Remote.controllerAxes.axes["right_x"].axis_position *
+                R_X_AXIS_SCALE_VAL), (Remote.controllerAxes.axes["left_y"].axis_position *
                 L_Y_AXIS_SCALE_VAL))
 
     except KeyboardInterrupt:
