1f096e3e01e87a825984208f82c96eef32b85ab7
David Taylor <dt@dt-mbp15.local>
2020-03-06 16:08:41 -0800

    renamed file

---
+++ RaspberryPi/test.py
@@ -0,0 +1,10 @@
+from serial import Serial
+import RPi.GPIO as GPIO
+import os, time
+
+GPIO.setmode(GPIO.BOARD)
+port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=2.5)
+
+while True:
+    rcv = port.read(100)
+    print(rcv)
